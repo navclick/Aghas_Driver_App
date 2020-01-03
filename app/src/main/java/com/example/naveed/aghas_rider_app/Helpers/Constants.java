e14edfd33f396b61ac9f86990d81a747ab9008ce
asadkhan <asa_khann@hotmail.com>
2020-01-03 15:10:13 +0500

    as

--- app/src/main/java/com/example/naveed/aghas_rider_app/Helpers/Constants.java
+++ app/src/main/java/com/example/naveed/aghas_rider_app/Helpers/Constants.java
@@ -6,7 +6,11 @@ import java.util.Map;
 public class Constants {
 
     // Network
-    public static String BASE_URL = "http://192.168.100.2:9091/";
+
+    public static String BASE_URL="http://aghasapi.insidedemo.com/";
+    //public static String BASE_URL = "http://192.168.100.2:9091/";
+    //public static String BASE_URL="http://apis.aghas.com.pk/";
+  //  public static String BASE_URL = "http://192.168.100.2:9091/";
     // public static String BASE_URL = "http://ocflexapi.insidedemo.com/";
     public final static  long CONNECTION_TIMEOUT = 25;
     public final static int DATA_SUCCESS = 1;
